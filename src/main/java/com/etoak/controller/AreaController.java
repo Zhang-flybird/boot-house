af4d09a8d09171a7506ea82207ead2f4ec249f97
huiliang <1245487997@qq.com>
2020-04-20 17:51:52 +0800

    配置swagger,删除swagger中datatype解决问题

--- src/main/java/com/etoak/controller/AreaController.java
+++ src/main/java/com/etoak/controller/AreaController.java
@@ -2,6 +2,9 @@ package com.etoak.controller;
 
 import com.etoak.bean.Area;
 import com.etoak.service.AreaService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,6 +27,9 @@ public class AreaController {
     AreaService areaService;
 
     @GetMapping("/queryByPid")
+    @ApiOperation(value = "根据父id查询地区列表",notes = "根据父id查询地区列表")
+    @ApiImplicitParam(value = "父id",name = "pid",required = false,
+            defaultValue = "0",paramType = "query")
     public List<Area> queryByPid(@RequestParam(required = false,defaultValue = "0")int pid){
         log.info("pid - {}",pid);
         return areaService.queryByPid(pid);
