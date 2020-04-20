7112a75ad05ac416b6eca193bac0ade6be96c1bf
huiliang <1245487997@qq.com>
2020-04-20 14:59:02 +0800

    dev分支开发的Dict.java

---
+++ src/main/java/com/etoak/bean/Dict.java
@@ -0,0 +1,13 @@
+package com.etoak.bean;
+
+import lombok.Data;
+
+@Data
+public class Dict {
+
+    private Integer id;
+    private String groupId;
+    private String name;
+    private String value;
+    private Integer sort;
+}
