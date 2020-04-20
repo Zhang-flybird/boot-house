b2e2c3dc084b5b1dced15cbd66b2a7dc021739ad
huiliang <1245487997@qq.com>
2020-04-20 11:46:39 +0800

    基本框架搭建完毕

--- src/main/java/com/etoak/HouseApp.java
+++ src/main/java/com/etoak/HouseApp.java
@@ -1,4 +1,11 @@
 package com.etoak;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class HouseApp {
+    public static void main(String[] args) {
+        SpringApplication.run(HouseApp.class, args);
+    }
 }
