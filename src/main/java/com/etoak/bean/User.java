12a55e6f394550113617727fc3e411f4112d0632
huiliang <1245487997@qq.com>
2020-04-20 14:54:14 +0800

    dev分支开发User.java

---
+++ src/main/java/com/etoak/bean/User.java
@@ -0,0 +1,14 @@
+package com.etoak.bean;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+    private Integer id;
+    private String name;
+    private String password;
+    private String gender;
+    private Integer age;
+    private String birthday;
+}
