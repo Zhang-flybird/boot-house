c9e9f9239b6f649c30dcb48968cbf6ca0e159112
huiliang <1245487997@qq.com>
2020-04-21 12:17:30 +0800

    添加add.html页面，增加SQL语句

--- sql/t_springmvc_dict.sql
+++ sql/t_springmvc_dict.sql
@@ -7,3 +7,15 @@ CREATE TABLE `t_springmvc_dict` (
   `sort` int(11) NOT NULL COMMENT '排序字段',
   PRIMARY KEY (`id`)
 );
+
+
+INSERT INTO `t_springmvc_dict` (`id`, `group_id`, `name`, `value`, `sort`) VALUES (null, 'rent_mode', '整租', '1', '1');
+INSERT INTO `t_springmvc_dict` (`id`, `group_id`, `name`, `value`, `sort`) VALUES (null, 'rent_mode', '合租', '2', '2');
+INSERT INTO `t_springmvc_dict` (`id`, `group_id`, `name`, `value`, `sort`) VALUES (null, 'house_type', '一居', '1', '1');
+INSERT INTO `t_springmvc_dict` (`id`, `group_id`, `name`, `value`, `sort`) VALUES (null, 'house_type', '二居', '2', '2');
+INSERT INTO `t_springmvc_dict` (`id`, `group_id`, `name`, `value`, `sort`) VALUES (null, 'house_type', '三居', '3', '3');
+INSERT INTO `t_springmvc_dict` (`id`, `group_id`, `name`, `value`, `sort`) VALUES (null, 'house_type', '四居及以上', '4', '4');
+INSERT INTO `t_springmvc_dict` (`id`, `group_id`, `name`, `value`, `sort`) VALUES (null, 'orientation', '东', '1', '1');
+INSERT INTO `t_springmvc_dict` (`id`, `group_id`, `name`, `value`, `sort`) VALUES (null, 'orientation', '西', '2', '2');
+INSERT INTO `t_springmvc_dict` (`id`, `group_id`, `name`, `value`, `sort`) VALUES (null, 'orientation', '南', '3', '3');
+INSERT INTO `t_springmvc_dict` (`id`, `group_id`, `name`, `value`, `sort`) VALUES (null, 'orientation', '北', '4', '4');
---
+++ src/main/java/com/etoak/controller/HouseController.java
@@ -0,0 +1,18 @@
+package com.etoak.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/house")
+public class HouseController {
+
+    /**
+     * 跳转到添加页面
+     * @return
+     */
+    @RequestMapping("/toAdd")
+    public String toAdd(){
+        return "house/add";
+    }
+}
